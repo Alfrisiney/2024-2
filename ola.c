d7dfc878f1cd91ea47f75652669d1060d7d2c14e
Alfrisiney <seniorito_cba@hotmail.com>
2024-10-05 00:20:21 +0000

    imprimindo variaveis

--- ola.c
+++ ola.c
@@ -2,5 +2,19 @@
 #include <stdlib.h>
 
 void main() {
+    int x = 0;//criando uma variavel inteira
+    printf("Valor de x: %d\n",x);
+    x++;//incrementando x em 1 unidade
+    printf("Valor de x: %d\n",x);
+    float y = 12.345678;//criando uma variavel real
+    printf("Valor de y: %f\n",y);
+    printf("Valor de y: %.2f\n",y);//imprimindo o valor de y com apenas 2 casas 
+    //Caractere deve estar entre apóstrofo
+    char sexo = 'M';//variavel que aceita um 1 caractere
+    printf("Sexo: %c\n",sexo);
+    //criando uma variavel String (texto)
+    char nome[30] = "Joao";
+    printf("Nome: %s\n",nome);
+    printf("Primeira letra: %c\n", nome[0]);
     printf("Olá Mundo!");
 }
